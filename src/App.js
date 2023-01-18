e78d0a65e14fe16d5430a027a5361a9367e93487
Batrawi <batrawi2000forever@gmail.com>
2023-01-18 23:43:59 +0200

    GSG React Bootcamp Tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import NamedComponent from "./Components/NamedComponent";
+import Tweet from "./Components/Tweet";
+import Counter from "./Components/counter";
+import Person from "./Components/Person";
+import Palette from "./Components/Palette";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Counter />
+      <NamedComponent name="battrawi" />
+      <div>
+        <Tweet
+          username="@john_doe"
+          name="John Doe"
+          date="Jan 18, 2021"
+          message="This is my first tweet!"
+        />
+        <Tweet
+          username="@jane_doe"
+          name="Jane Doe"
+          date="Jan 19, 2021"
+          message="Just learned how to use React!"
+        />
+        <Tweet
+          username="@jim_smith"
+          name="Jim Smith"
+          date="Jan 20, 2021"
+          message="Excited to start building my own apps!"
+        />
+      </div>
+      <div>
+        <Person
+          name="John Doe"
+          age={25}
+          hobbies={["reading", "swimming", "traveling"]}
+        />
+        <Person
+          name="Jane Smith"
+          age={19}
+          hobbies={["painting", "singing", "cooking"]}
+        />
+        <Person
+          name="Jim Brown"
+          age={30}
+          hobbies={["dancing", "programming", "biking"]}
+        />
+      </div>
+      <div>
+        <Palette />
+      </div>
     </div>
   );
 }
